3f00fcaca156889f687f8b3b1fd8302623f48d3d
RenMal88 <renzomalzone8@gmail.com>
2026-04-06 21:29:43 -0300

    first commit

---
+++ Ficha 0/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Tarjeta from "./Components/Ui/Tarjeta";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div>
+      <Tarjeta nombre></Tarjeta>
+    </div>
+  );
+}
+
+export default App;
